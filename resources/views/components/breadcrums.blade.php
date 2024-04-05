a5f943c0b7e435e264721a8bbb301cd9e51dfd96
johnpatrick254 <jpattrick538@gmail.com>
2024-04-05 19:09:53 +0300

    feat: add breadcrumb

---
+++ resources/views/components/breadcrums.blade.php
@@ -0,0 +1,13 @@
+ <nav>
+     <ul class="flex space-x-1 py-2">
+         <li>
+             <a href="http:/" target="_self" >Home</a>
+         </li>
+         @foreach ($links as $label => $link)
+             <li>/</li>
+             <li>
+                 <a href="{{ $link }}" target="_self" rel="noopener noreferrer"> {{$label}} </a>
+             </li>
+         @endforeach
+     </ul>
+ </nav>
