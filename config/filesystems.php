ca9e13e2a4d7569c9f15312103a637c800690977
johnpatrick254 <jpattrick538@gmail.com>
2024-04-08 17:37:20 +0300

    feat:configure local filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,13 @@ return [
             'throw' => false,
         ],
 
+        'private'=>[
+            'driver'=>'local',
+            'root' => storage_path('app/private'),
+            'visibility' => 'public',
+            'throw' => false,
+            ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
