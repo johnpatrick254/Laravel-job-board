075b6b02942953f45c9040e113cda5690fe02ecd
johnpatrick254 <jpattrick538@gmail.com>
2024-05-27 17:14:00 +0300

    chore: setup demo user

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
             <!-- Email Address -->
             <div>
                 <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="'test@mail.com'"
                     required autofocus autocomplete="username" />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
@@ -19,9 +19,9 @@
                 <x-input-label for="password" :value="__('Password')" />
 
                 <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
-                    autocomplete="current-password" />
+                    autocomplete="current-password" :value="'password'" />
 
-                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <x-input-error :messages="$errors->get('password')" class="mt-2"  />
             </div>
 
             <!-- Remember Me -->
